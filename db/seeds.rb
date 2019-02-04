34d87bee0ae21b95e00ba760c14f1e111c1a3d46
gillsinaglass <rdgill95@gmail.com>
2019-02-04 12:39:31 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+artist1 = Artist.create(name: "Lil Jon")
+artist2 = Artist.create(name: "Maroon 5")
+artist3 = Artist.create(name: "Ray Jay")
+Song.create(title:"Get Low", artist_id: artist1.id)
+Song.create(title: "Girls like you", artist_id: artist2.id)
+Song.create(title: "Hit it first", artist_id: artist3.id)
