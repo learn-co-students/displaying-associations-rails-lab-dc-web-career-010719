a5443cc801b9401e18bcd23498935ca30beba46b
shannon-nabors <shannon.nabors@gmail.com>
2019-02-04 17:40:34 -0500

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+
+Artist.create(name: "Aretha Franklin")
+Artist.create(name: "Steely Dan")
+
+Song.create(title: "Respect", artist_id: 7)
+Song.create(title: "Spanish Harlem", artist_id: 7)
+Song.create(title: "You're All I Need To Get By", artist_id: 7)
+Song.create(title: "My Old School", artist_id: 8)
+Song.create(title: "Bad Sneakers", artist_id: 8)
+Song.create(title: "Josie", artist_id: 8)
